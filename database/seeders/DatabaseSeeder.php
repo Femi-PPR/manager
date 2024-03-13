c78f4bc4cf5b4a86a7a755e860961e4b7e010196
Femi Oladapo <femioladapo3@gmail.com>
2024-03-13 09:30:26 +0000

    database seeder, seeds the employee, company and user database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,5 +19,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@admin.com',
             'password' => Hash::make('password'),
         ]);
+        $this->call([
+            EmployeeSeeder::class,
+        ]);
     }
 }
