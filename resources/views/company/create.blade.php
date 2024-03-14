81143451c94583df5350b5d9c88ab47bfb1da281
Femi Oladapo <femioladapo3@gmail.com>
2024-03-14 08:42:26 +0000

    Added form for adding companies

---
+++ resources/views/company/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container col-6 form-bg py-4 px-4 rounded">
+        <form action="{{ route('company.store') }}" method="post" enctype="multipart/form-data">
+            @csrf
+
+            <h2 class="mb-4 mt-1">Add Company</h2>
+            <x-input name="name" label="Company Name <span class='text-danger'>*</span>" />
+            <x-input name="email" label="Email" />
+            <x-input name="website" label="Website" />
+            <x-input name="logo" label="Logo" type="file" />
+            
+            <button
+                type="submit"
+                class="btn btn-primary"
+            >
+                Submit
+            </button>
+            
+        </form>
+    </div>
+    
+@endsection
