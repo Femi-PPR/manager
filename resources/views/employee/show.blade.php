4332cf03868d3aadc75c65a51af322b7556c8ab2
Femi Oladapo <femioladapo3@gmail.com>
2024-03-14 20:57:00 +0000

    added button to edit and delete an employee's details

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -3,26 +3,42 @@
 @section('content')
     <div class="container">
         <div
-            class="row justify-content-center align-items-center g-2"
+            class=" row justify-content-center align-items-center g-2"
         >
-            <div class="col-4"><img src="{{ asset('backgrounds/raphael--employee.svg') }}" class="object-fit-cover img-thumbnail w-100 h-100" alt=""></div>
-            <div class="col-8">
+            <div class="col-md-4"><img src="{{ asset('backgrounds/raphael--employee.svg') }}" class="object-fit-cover img-thumbnail w-100 h-100" alt=""></div>
+            <div class="col-md-8">
                 <!-- Hover added -->
                 <div class="list-group list-group-flush">
                     <h1 class="list-group-item display-3 py-4">{{ $employee->first_name . " " . $employee->last_name }}</h1>
                     <p class="list-group-item">
-                        @if (isset($employee->company))
+                        @isset($employee->company)
                             Works for {{ $employee->company->name }}
                         @else
                             Is a Free Agent
                         @endif
                     </p>
+                    @isset($employee->email)
                     <p class="list-group-item">Email: {{ $employee->email }}</p>
+                    @endisset
+                    @isset($employee->telephone)
                     <p class="list-group-item">Phone Number: {{ $employee->telephone }}</p>
-      
+                    @endisset
+                    @auth
+                    <div class="list-group-item d-grid gap-3 d-md-flex">
+                        <a href="{{ route('employee.edit', ['employee' => $employee->id]) }}" class="btn btn-outline-primary">Edit</a>
+                        <form action="{{ route('employee.destroy', ['employee' => $employee->id]) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button class="btn w-100 btn-outline-danger">Delete</button>
+                        </form>
+                    </div>
+                    @endauth
                 </div>
                 
             </div>
         </div>
     </div>
+    @if (session()->has('success'))
+        <x-toast :msg="session('success')" />
+    @endif
 @endsection
