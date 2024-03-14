ce1f9b7c9fc40f76635dfa8663986eb181eb5320
Femi Oladapo <femioladapo3@gmail.com>
2024-03-14 20:56:17 +0000

    added styles for smaller viewports

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -5,10 +5,10 @@
         <div
             class="row justify-content-center align-items-center g-2"
         >
-            <div class="col-4 align-self-stretch">
+            <div class="col-md-4 align-self-stretch">
                 <img src="{{ empty($company->logo)? asset('backgrounds/icon-park-solid--building-two.svg') : asset('storage/' . $company->logo) }}" class="object-fit-cover img-thumbnail w-100 h-100" alt="">
             </div>
-            <div class="col-8">
+            <div class="col-md-8">
                 <!-- Hover added -->
                 <div class="list-group list-group-flush">
                     <h1 class="list-group-item display-3 py-4">{{ $company->name }}</h1>
@@ -24,7 +24,7 @@
                             <form action="{{ route('company.destroy', ['company' => $company->id]) }}" method="post">
                                 @csrf
                                 @method('delete')
-                                <button class="btn btn-outline-danger">Delete</button>
+                                <button class="btn w-100 btn-outline-danger">Delete</button>
                             </form>
                         </div>
                     @endauth
@@ -33,6 +33,9 @@
             </div>
         </div>
         <h1 class="display-6 pt-5 pb-2">Employees</h1>
+        @auth
+            <a href="{{ route('employee.create', ['company_id'=> $company->id]) }}" class="btn btn-primary mb-4"> Add Employee to Company </a>
+        @endauth
         <div class="list-group">
             @if ($company->employees->count() > 0)
                 @foreach ($company->employees as $employee)
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row pt-5 justify-content-center align-items-center g-4 welcome-cards">
-            <div class="col">
+            <div class="col-md">
                 <div class="card">
                     <img class="card-img-top object-fit-cover" src="backgrounds/companies.jpg" alt="Title" />
                     <div class="card-body">
@@ -12,7 +12,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col">
+            <div class="col-md">
                 <div class="card">
                     <img class="card-img-top object-fit-cover" src="backgrounds/employees.jpg" alt="Title" />
                     <div class="card-body">
