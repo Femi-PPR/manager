c840e007cb7030fcce0e900a97511fb70311da28
Femi Oladapo <femioladapo3@gmail.com>
2024-03-14 08:38:21 +0000

    have added it so that null value in company data are not shown

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -10,21 +10,29 @@
                 <!-- Hover added -->
                 <div class="list-group list-group-flush">
                     <h1 class="list-group-item display-3 py-4">{{ $company->name }}</h1>
+                    @isset($company->email)  
                     <p class="list-group-item">Email: {{ $company->email }}</p>
+                    @endisset
+                    @isset($company->website)  
                     <p class="list-group-item">Website: {{ $company->website }}</p>
+                    @endisset
       
                 </div>
                 
             </div>
         </div>
-        <h1 class="display-6">Employees</h1>
+        <h1 class="display-6 pt-5 pb-2">Employees</h1>
         <div class="list-group">
-            @foreach ($company->employees as $employee)
-                <a href="/employees/{{ $employee->id }}" 
-                    class="list-group-item d-flex justify-content-between align-items-center">
-                    {{ $employee->first_name . " " . $employee->last_name}} 
-                </a>
-            @endforeach
+            @if ($company->employees->count() > 0)
+                @foreach ($company->employees as $employee)
+                    <a href="/employees/{{ $employee->id }}" 
+                        class="list-group-item d-flex justify-content-between align-items-center">
+                        {{ $employee->first_name . " " . $employee->last_name}} 
+                    </a>
+                @endforeach
+            @else
+                <p class="fs-5">This company has no employees</p>
+            @endif
         </div>
     </div>
 @endsection
