b315ca97136a2b695010c52f7b7665702c0988c5
Femi Oladapo <femioladapo3@gmail.com>
2024-03-14 08:42:00 +0000

    added padding to top of cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center align-items-center g-4 welcome-cards">
+        <div class="row pt-5 justify-content-center align-items-center g-4 welcome-cards">
             <div class="col">
                 <div class="card">
                     <img class="card-img-top object-fit-cover" src="backgrounds/companies.jpg" alt="Title" />
