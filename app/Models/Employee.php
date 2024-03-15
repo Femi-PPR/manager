c82abb0fc6534f14aa1efbcc98cfb9c649e35009
Femi Oladapo <femioladapo3@gmail.com>
2024-03-15 16:47:19 +0000

    added employment history to Employee model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -17,8 +17,28 @@ class Employee extends Model
         "telephone"
     ];
 
+    public static function boot()
+    {
+        parent::boot();
+
+        static::updating(function ($employee) {
+            $prev_company_id = $employee->fresh()->company_id;
+            if ($prev_company_id != $employee->company_id) {
+                PreviousEmployment::create([
+                    'employee_id' => $employee->id,
+                    'company_id' => $prev_company_id,
+                ]);
+            }
+        });
+    }
+
     public function company()
     {
-        return $this->belongsTo(Company::class);
+        return $this->belongsTo(Company::class)->withTrashed();
+    }
+
+    public function employmentHistory()
+    {
+        return $this->hasMany(PreviousEmployment::class)->latest()->with('company');
     }
 }
