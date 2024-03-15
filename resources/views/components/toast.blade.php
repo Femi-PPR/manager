9f7b3c77c2570b0481afd985bc86faff8bc590be
Femi Oladapo <femioladapo3@gmail.com>
2024-03-15 16:50:55 +0000

    updated toast to have higher z-index

--- resources/views/components/toast.blade.php
+++ resources/views/components/toast.blade.php
@@ -1,6 +1,6 @@
 @props(['msg'])
 
-<div class="toast toast-success show position-fixed bottom-0 end-0 mb-3 me-3" role="alert" aria-live="assertive" aria-atomic="true">
+<div class="toast toast-success show position-fixed z-3 bottom-0 end-0 mb-3 me-3" role="alert" aria-live="assertive" aria-atomic="true">
     <div class="toast-header">
         <strong class="me-auto">Success</strong>
         <button
